36ff8b6aaf9ce232b6b14cdb0678ab48fe454535
HaneulSky <tanya0610@naver.com>
2021-09-08 22:03:46 +0300

    начата верстка таблицы, пока не добавлены стили и налпись сверху, говорящая сколько человек тусанут со мной сегодня

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import api from "../api";
 
 const Users = () => {
-  api.users.fetchAll;
-  return <h1>Users</h1>;
+  const [users, setUsers] = useState(api.users.fetchAll());
+  console.log(users);
+
+  return (
+    <React.Fragment>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Имя</th>
+            <th scope="col">Качества</th>
+            <th scope="col">Профессия</th>
+            <th scope="col">Встретился, раз</th>
+            <th scope="col">Оценка</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {users.map((user) => (
+              <tr>
+                <td key={user._id}>{user.name}</td>
+                <td key={user._id}>
+                  {user.qualities.map((quality) => quality.name)}
+                </td>
+                <td key={user._id}>{user.profession.name}</td>
+                <td key={user._id}>{user.completedMeetings}</td>
+                <td key={user._id}>{user.rate}</td>
+              </tr>
+            ))}
+          </tr>
+        </tbody>
+      </table>
+    </React.Fragment>
+  );
 };
 
 export default Users;
