9ff4320bf10f5ca20b2c7ba067dbeae9cb2a1bba
HaneulSky <tanya0610@naver.com>
2021-09-19 15:23:48 +0300

    настроен еслинт и преттиер, прописаны типы для всех данных, убраны ошибки из консоли

--- src/app.jsx
+++ src/app.jsx
@@ -4,43 +4,43 @@ import SearchStatus from "./components/searchStatus";
 import api from "./api";
 
 const App = () => {
-  const [users, setUsers] = useState(api.users.fetchAll());
-  console.log(users.length);
+    const [users, setUsers] = useState(api.users.fetchAll());
+    console.log(users.length);
 
-  const handleDelete = (userID) => {
-    return setUsers(users.filter((user) => user._id !== userID));
-  };
+    const handleDelete = (userID) => {
+        return setUsers(users.filter((user) => user._id !== userID));
+    };
 
-  const renderPhrase = (num) => {
-    if (num > 1 && num < 5) {
-      return `${users.length} человека тусанут с тобой сегодня`;
-    } else if (users.length === 0) {
-      return "Никто не тусанет с тобой сегодня";
-    } else return `${users.length} человек тусанет с тобой сегодня`;
-  };
+    const renderPhrase = (num) => {
+        if (num > 1 && num < 5) {
+            return `${users.length} человека тусанут с тобой сегодня`;
+        } else if (users.length === 0) {
+            return "Никто не тусанет с тобой сегодня";
+        } else return `${users.length} человек тусанет с тобой сегодня`;
+    };
 
-  const toggleBookmark = (userId) => {
-    setUsers(
-      users.filter((user) => {
-        if (user._id === userId) {
-          user.status = !user.status;
-          return user;
-        }
-        return user;
-      })
+    const toggleBookmark = (userId) => {
+        setUsers(
+            users.filter((user) => {
+                if (user._id === userId) {
+                    user.status = !user.status;
+                    return user;
+                }
+                return user;
+            })
+        );
+    };
+
+    return (
+        <>
+            <SearchStatus onPhrase={renderPhrase} />
+            <Users
+                users={users}
+                onDelete={handleDelete}
+                onBookmark={toggleBookmark}
+            />
+        </>
     );
-  };
-
-  return (
-    <div>
-      <SearchStatus onPhrase={renderPhrase} lengthUsers={users.length} />
-      <Users
-        users={users}
-        onDelete={handleDelete}
-        onBookmark={toggleBookmark}
-      />
-    </div>
-  );
 };
 
 export default App;
--- src/components/searchStatus.jsx
+++ src/components/searchStatus.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 const searchStatus = ({ onPhrase, lengthUsers }) => {
-  return (
-    <span className={"badge bg-" + (lengthUsers > 0 ? "primary" : "danger")}>
-      {onPhrase(lengthUsers)}
-    </span>
-  );
+    return (
+        <span
+            className={"badge bg-" + (lengthUsers > 0 ? "primary" : "danger")}
+        >
+            {onPhrase(lengthUsers)}
+        </span>
+    );
 };
 
 export default searchStatus;
